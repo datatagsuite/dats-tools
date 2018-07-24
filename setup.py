4fbf10ee23c76f63d262e3eff22b1d135f0eba02
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2018-07-24 14:30:54 +0100

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='dats',
-    version='2.1',
+    name='dats-tools',
+    version='0.1',
     packages=['dats'],
     description='DATS',
     author='agbeltran',
     author_email='alejandra.gonzalez.beltran@gmail.com',
-    url='http://www.biocaddie.org/',
+    url='http://github.com/datatagsuite',
     test_suite='tests'
 )
