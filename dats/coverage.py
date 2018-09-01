81636bb00e1e154bf8ec961b89930a44ec6c00f8
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2018-09-01 21:06:37 +0100

    Method to get all the semantic mappings

--- dats/coverage.py
+++ dats/coverage.py
@@ -28,28 +28,39 @@ class Coverage:
     def json_schema_context_mapping(schema, *contexts):
         "Method to retrieve the mapping between a schema and multiple context files"
         mappings = {}
-        terms = []
 
         for field in schema["properties"]:
+            terms = []
             if field not in Coverage.jsonld_ignored_keys:
                 for context in contexts:
                     #print(context)
                     if field in context["@context"]:
-                        terms.append(context["@context"][field])
-
+                        if "@id" in context["@context"][field]:
+                            terms.append(context["@context"][field]["@id"])
+                        else:
+                            terms.append(context["@context"][field])
+                    else:
+                        terms.append("")
+                if field in mappings:
+                    previous_terms = mappings[field]
+                    terms.append(previous_terms)
                 mappings.update( { field : terms })
         print(mappings)
 
 if __name__ == "__main__":
     schema_filename = "dataset_schema.json"
-    context_filename = "dataset_obo_context.jsonld"
+    obo_context_filename = "dataset_obo_context.jsonld"
+    sdo_context_filename = "dataset_sdo_context.jsonld"
     schemas_path = os.path.join(os.path.dirname(__file__), "../json-schemas")
     obo_contexts_path = os.path.join(os.path.dirname(__file__), "../contexts/obo")
+    sdo_contexts_path = os.path.join(os.path.dirname(__file__), "../contexts/sdo")
     schema_file = open(os.path.join(schemas_path, schema_filename))
-    context_file = open(os.path.join(obo_contexts_path,context_filename))
+    obo_context_file = open(os.path.join(obo_contexts_path,obo_context_filename))
+    sdo_context_file = open(os.path.join(sdo_contexts_path, sdo_context_filename))
     schema = json.load(schema_file)
-    context = json.load(context_file)
-    print(Coverage.json_schema_context_coverage(schema, context))
+    obo_context = json.load(obo_context_file)
+    sdo_context = json.load(sdo_context_file)
+    print(Coverage.json_schema_context_coverage(schema,obo_context))
 
 
-    Coverage.json_schema_context_mapping(schema, context)
+    Coverage.json_schema_context_mapping(schema, sdo_context, obo_context)
